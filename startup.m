15a138399554613bf00c82180aeb64af4ef9c04d
Tanmay Das <tdas@mathworks.com>
2023-06-28 16:48:44 +0530

    print log in output

--- startup.m
+++ startup.m
@@ -1,8 +1,9 @@
 
-temp = getenv("RUNNER_TEMP");
-filepath = fullfile(temp, 'myfile.txt')
-fileID = fopen(filepath, 'w');
-fprintf(fileID, 'Hello, world!\n');
-fprintf(fileID, 'This is line 2.\n');
-fprintf(fileID, 'And this is line 3.\n');
-fclose(fileID);
+disp('This is a MATLAB file')
+% temp = getenv("RUNNER_TEMP");
+% filepath = fullfile(temp, 'myfile.txt')
+% fileID = fopen(filepath, 'w');
+% fprintf(fileID, 'Hello, world!\n');
+% fprintf(fileID, 'This is line 2.\n');
+% fprintf(fileID, 'And this is line 3.\n');
+% fclose(fileID);
